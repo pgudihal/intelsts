d8d9f3b59611f73c4b0337056014d3d99bec77ce
Prateek Gudihal <pratik.gudihal@gmail.com>
2011-11-04 01:09:28 -0400

    removed some migrations

--- db/migrate/20111001222541_remove_application_id_from_score.rb
+++
@@ -1,9 +0,0 @@
-class RemoveApplicationIdFromScore < ActiveRecord::Migration
-  def up
-    remove_column :scores, :application_id
-  end
-
-  def down
-    add_column :scores, :application_id, :integer
-  end
-end
--- db/migrate/20111001222810_remove_assignment_id_from_score.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAssignmentIdFromScore < ActiveRecord::Migration
-  def up
-    remove_column :scores, :assignment_id
-  end
-
-  def down
-    add_column :scores, :assignment_id, :integer
-  end
-end
